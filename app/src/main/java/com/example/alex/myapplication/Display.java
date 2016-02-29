6f63eb0dd95a12dc74a8fc6754a2ae2b6e1771ce
Alexfc3 <alex.centeno3332@gmail.com>
2016-02-28 21:37:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alex/myapplication/Display.java
@@ -0,0 +1,16 @@
+package com.example.alex.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alex on 2/28/2016.
+ */
+public class Display extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.display);
+    }
+}
